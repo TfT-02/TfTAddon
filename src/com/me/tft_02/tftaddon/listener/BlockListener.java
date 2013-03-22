ae69d9ba45a01c582d27dd6a1fc8d104348ac595
TfT_02 <tft_02@hotmail.com>
2013-03-22 23:19:21 +0100

    Added insta-break obisidian for SuperBreaker

--- src/com/me/tft_02/tftaddon/listener/BlockListener.java
+++ src/com/me/tft_02/tftaddon/listener/BlockListener.java
@@ -1,12 +1,16 @@
 package com.me.tft_02.tftaddon.listener;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.inventory.ItemStack;
 
+import com.gmail.nossr50.api.AbilityAPI;
 import com.me.tft_02.tftaddon.TfTAddon;
 import com.me.tft_02.tftaddon.skills.Repair;
 import com.me.tft_02.tftaddon.util.ItemChecks;
@@ -28,5 +32,29 @@ public class BlockListener implements Listener {
         if (ItemChecks.isRepairable(is)) {
             repair.checkRepair(player, is);
         }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+    public void onBlockDamageHigher(BlockDamageEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+
+        if (!plugin.getConfig().getBoolean("Skills.Mining.SuperBreaker_InstaBreak_Obsidian")) {
+            return;
+        }
+
+        if (!ItemChecks.isPickaxe(player.getItemInHand())) {
+            return;
+        }
+
+        if (!AbilityAPI.superBreakerEnabled(player)) {
+            return;
+        }
+
+        if (block.getType() != Material.OBSIDIAN) {
+            return;
+        }
+
+        event.setInstaBreak(true);
     }
 }
