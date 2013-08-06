235b0fad747978c4127eed07369a39e925abed60
TfT_02 <tft_02@hotmail.com>
2013-08-07 01:35:12 +0200

    Cleanup the config file

--- src/com/me/tft_02/tftaddon/config/Config.java
+++ src/com/me/tft_02/tftaddon/config/Config.java
@@ -3,7 +3,7 @@ package com.me.tft_02.tftaddon.config;
 import java.util.List;
 
 public class Config extends AutoUpdateConfigLoader {
-    private static  Config instance;
+    private static Config instance;
 
     private Config() {
         super("config.yml");
@@ -23,11 +23,10 @@ public class Config extends AutoUpdateConfigLoader {
     /* @formatter:off */
 
     /* GENERAL SETTINGS */
-    public String getLocale() { return config.getString("General.Locale", "en_us"); }
-    public int getSaveInterval() { return config.getInt("General.Save_Interval", 10); }
+//    public String getLocale() { return config.getString("General.Locale", "en_us"); }
+//    public int getSaveInterval() { return config.getInt("General.Save_Interval", 10); }
     public boolean getStatsTrackingEnabled() { return config.getBoolean("General.Stats_Tracking", true); }
     public boolean getUpdateCheckEnabled() { return config.getBoolean("General.Update_Check", true); }
-    public boolean getPreferBeta() { return config.getBoolean("General.Prefer_Beta", false); }
     public boolean getVerboseLoggingEnabled() { return config.getBoolean("General.Verbose_Logging", false); }
     public boolean getConfigOverwriteEnabled() { return config.getBoolean("General.Config_Update_Overwrite", true); }
 
--- src/com/me/tft_02/tftaddon/runnables/UpdateCheckerTask.java
+++ src/com/me/tft_02/tftaddon/runnables/UpdateCheckerTask.java
@@ -3,7 +3,6 @@ package com.me.tft_02.tftaddon.runnables;
 import com.me.tft_02.tftaddon.TfTAddon;
 import com.me.tft_02.tftaddon.util.UpdateChecker;
 
-
 /**
  * Async task
  */
